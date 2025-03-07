e9e643024df9b08355a0ae0d88f7de84b1015896
RuthMaria <ruth-15rms@hotmail.com>
2025-03-06 23:37:58 -0300

    first commit

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+// é o mesmo que usar o [Route("Filme")], pois ele já pega o nome que está no controler. Caso mude o nome do controler, não precisaria alterar o route.
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    public static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]    
+    public void Adciciona([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Duracao);
+    }
+}
